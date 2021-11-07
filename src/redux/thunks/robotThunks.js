9b288d1bf82c883ce76cd977f0e1e3b5e07531e3
David <david.llobet6@gmail.com>
2021-11-07 19:11:28 +0100

    Create deleteRobotThunk

--- src/redux/thunks/robotThunks.js
+++ src/redux/thunks/robotThunks.js
@@ -1,9 +1,17 @@
-import { loadRobotsAction } from "../actions/actionCreators";
+import { deleteRobotAction, loadRobotsAction } from "../actions/actionCreators";
 
 export const loadRobotsThunk = () => {
   return async (dispatch) => {
     const response = await fetch("https://robots2.herokuapp.com/robots");
     const robots = await response.json();
     dispatch(loadRobotsAction(robots));
+  };
+};
+
+export const deleteRobotThunk = (id) => {
+  return async (dispatch) => {
+    const response = await fetch(`https://robots2.herokuapp.com/robots/${id}`);
+    const deletedRobot = await response.json();
+    dispatch(deleteRobotAction(deletedRobot));
   };
 };
