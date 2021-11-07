e4261c67ae0307e89c297de7d9ac7941ff103e47
David <david.llobet6@gmail.com>
2021-11-07 13:32:45 +0100

    Create and implement hook useRobot

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,29 +1,27 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { loadRobotsThunk } from "../../redux/thunks/robotThunks";
+import useRobots from "../../hooks/useRobots";
 import Robot from "../Robot/Robot";
 
 const List = () => {
-  const robots = useSelector(({ robots }) => robots);
+  const { robots } = useRobots();
   console.log(robots);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadRobotsThunk());
-  }, [dispatch]);
+
   return (
-    <ul className="robots-list">
-      <h2>My Robots:</h2>
-      {robots.map((robot) => (
-        <Robot
-          key={robot._id}
-          name={robot.name}
-          image={robot.url}
-          speed={robot.characteristics.speed}
-          stamina={robot.characteristics.stamina}
-          creationDate={robot.characteristics.creationDate}
-        />
-      ))}
-    </ul>
+    <>
+      <pre>{JSON.stringify(robots, null, 2)}</pre>
+      <ul className="robots-list">
+        <h2>My Robots:</h2>
+        {robots.map((robot) => (
+          <Robot
+            key={robot._id}
+            name={robot.name}
+            image={robot.url}
+            speed={robot.characteristics.speed}
+            stamina={robot.characteristics.stamina}
+            creationDate={robot.characteristics.creationDate}
+          />
+        ))}
+      </ul>
+    </>
   );
 };
 
---
+++ src/hooks/useRobots.js
@@ -0,0 +1,15 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadRobotsThunk } from "../redux/thunks/robotThunks";
+
+const useRobots = () => {
+  const robots = useSelector(({ robots }) => robots);
+  const dispatch = useDispatch();
+  const loadRobots = useCallback(() => {
+    dispatch(loadRobotsThunk());
+  }, [dispatch]);
+
+  return { robots, dispatch, loadRobots };
+};
+
+export default useRobots;
