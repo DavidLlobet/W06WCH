411bb102c3bee1ba3f37614bf86e40c5aecf4299
David <david.llobet6@gmail.com>
2021-11-07 15:01:15 +0100

    Create test-utils.js

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+describe("Given an App component", () => {
+  describe("When it renders and it contains some robots in the database", () => {
+    test("Then it should render a robot card", async () => {
+      renderWithProviders(<App />);
+    });
+  });
+});
