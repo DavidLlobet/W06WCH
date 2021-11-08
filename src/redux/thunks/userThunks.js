298476bbacc4d84b9f3a95cb32579c86879d4549
David <david.llobet6@gmail.com>
2021-11-08 20:31:20 +0100

    Modified userThunks.js

--- src/redux/thunks/userThunks.js
+++ src/redux/thunks/userThunks.js
@@ -2,8 +2,11 @@ import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { userLoginAction } from "../actions/actionCreators";
 
-export const loginUserThunk = (user, url) => async (dispatch) => {
-  const response = await axios.post(url, user);
+export const loginUserThunk = (user) => async (dispatch) => {
+  const response = await axios.post(
+    process.env.REACT_APP_URL + user.Paths.login,
+    user
+  );
   if (response.status === 200) {
     const token = response.data.token;
     const user = jwtDecode(token);
